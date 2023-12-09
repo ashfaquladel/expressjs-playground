7dfa28cbc45878ea49c63325469b52d6b21b626f
Ashfaqul Adel <ashfaquladel@gmail.com>
2023-12-10 00:00:08 +0600

    Add dummy model and POST API test

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
+let dummy = "500"
+
 app.get('/', (req, res) =>
     res.send('Hello, World!')
 )
+
+app.post('/update/dummy', (req, res) => {
+    const { newValue } = req.body
+    dummy = newValue
+    console.log(dummy)
+    res.send('Updated!')
+})
 
 app.listen(port, () => console.log("Server start hoise"))
